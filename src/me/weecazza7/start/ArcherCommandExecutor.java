dd1bd0cc7fb710fe342a8c0e35456ae78f916c58
imjackjack <jack@Jacks_PC>
2013-04-21 23:21:19 +0100

    update

--- src/me/weecazza7/start/ArcherCommandExecutor.java
+++ src/me/weecazza7/start/ArcherCommandExecutor.java
@@ -1,5 +1,98 @@
 package me.weecazza7.start;
 
-public class ArcherCommandExecutor {
-	//
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class ArcherCommandExecutor implements CommandExecutor {
+	private ArcherCommandExecutor myExecutor;
+	
+	public ArcherCommandExecutor(MainPvP plugin) {
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player p = (Player) sender;
+		Inventory pi = p.getInventory();
+		p.sendMessage(ChatColor.GREEN + "You are now a Archer!");
+		p.getInventory().clear();
+	    pi.addItem(getfood());
+	    pi.addItem(getsword());
+	    p.getInventory().setHelmet(gethelm());
+	    p.getInventory().setChestplate(getchest());
+	    p.getInventory().setLeggings(getlegs());
+	    p.getInventory().setBoots(getboot());
+	    ItemStack getSoup = new ItemStack(Material.MUSHROOM_SOUP, 1);
+	    for(int i=1; i <=34; i++)
+	    {
+	    pi.addItem(getSoup);
+	    }
+	    
+		
+		return false;
+		
+	}
+	public static ItemStack getfood(){
+		ItemStack itemStack = new ItemStack(Material.COOKED_BEEF, 5);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName("§bThe only way!");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+	public static ItemStack getsword(){
+		ItemStack itemStack = new ItemStack(Material.DIAMOND_SWORD);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.addEnchant(Enchantment.DAMAGE_ALL, 1, true);
+		itemMeta.setDisplayName("§bThe Knight's Sharp Sword!");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+	public static ItemStack gethelm(){
+		ItemStack itemStack = new ItemStack(Material.DIAMOND_HELMET);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName("§bThe Knight's Helm");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+	public static ItemStack getchest(){
+		ItemStack itemStack = new ItemStack(Material.DIAMOND_CHESTPLATE);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName("§bThe Knight's Chestplate");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+	public static ItemStack getlegs(){
+		ItemStack itemStack = new ItemStack(Material.DIAMOND_LEGGINGS);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName("§bThe Knight's Pants");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+	public static ItemStack getboot(){
+		ItemStack itemStack = new ItemStack(Material.DIAMOND_BOOTS);
+		ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName("§bThe Knight's Boots");
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
+
+	public ArcherCommandExecutor getMyExecutor() {
+		return myExecutor;
+	}
+
+	public void setMyExecutor(ArcherCommandExecutor myExecutor) {
+		this.myExecutor = myExecutor;
+	}
+
+
+	
+
 }
--- src/me/weecazza7/start/MainPvP.java
+++ src/me/weecazza7/start/MainPvP.java
@@ -15,80 +15,16 @@ public class MainPvP extends JavaPlugin {
 		PluginManager manager = this.getServer().getPluginManager();
 		manager.registerEvents(new OnJoinEventListener(), this);
 		manager.registerEvents(new SignEventListener(), this);
+		manager.registerEvents(new NoDropDeadEventListener(), this);
+		manager.registerEvents(new NoDropEventListener(), this);
+		manager.registerEvents(new SoupEventListener(), this);
 		getCommand("kit").setExecutor(new KitCommandExecutor(this));
 		getCommand("tank").setExecutor(new TankCommandExecutor(this));
+		getCommand("archer").setExecutor(new ArcherCommandExecutor(this));
 		
 	}
 	public void onDisable() {
 		Bukkit.getServer().getLogger().info("Disabled");
 		
-	}
-	
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		
-		if(!(sender instanceof Player)) {
-			sender.sendMessage("ERROR! Player use only!");
-			return true;
-			
-		}
-		
-		Player player = (Player) sender;
-		
-		if(cmd.getName().equalsIgnoreCase("heal")) {
-			if (args.length == 0) {
-				player.setHealth(20);
-				player.sendMessage(ChatColor.GREEN + "You have been Healed!");
-				return true;	
-		}
-		Player target = Bukkit.getServer().getPlayer(args[0]);
-		if (target == null) {
-			player.sendMessage(ChatColor.RED + "Could not find player");
-			return true;
-		}
-		target.setHealth(20);
-		target.sendMessage(ChatColor.BLUE + "You have been healed");
-		player.sendMessage(ChatColor.BLUE + target.getName() + " Healed!");
-	}
-		
-		
-		if(cmd.getName().equalsIgnoreCase("kill")) {
-			if (args.length == 0) {
-				player.setHealth(0);
-				player.sendMessage(ChatColor.GREEN + "You have been Killed");
-				return true;	
-		}
-		Player target = Bukkit.getServer().getPlayer(args[0]);
-		if (target == null) {
-			player.sendMessage(ChatColor.RED + "Could not find player");
-			return true;
-		}
-		target.setHealth(0);
-		target.sendMessage("You have been killed");
-		player.sendMessage(ChatColor.GREEN + target.getName() + " killed!");
-	}
-		
-		
-		if(cmd.getName().equalsIgnoreCase("feed")) {
-			if (args.length == 0) {
-				player.setFoodLevel(20);
-				player.sendMessage(ChatColor.GREEN + "You have been fed");
-				return true;	
-		}
-		Player target = Bukkit.getServer().getPlayer(args[0]);
-		if (target == null) {
-			player.sendMessage(ChatColor.RED + "Could not find player");
-			return true;
-		}
-		target.setFoodLevel(20);
-		target.sendMessage(ChatColor.BLUE + "You have been fed");
-		player.sendMessage(ChatColor.BLUE + target.getName() + " fed!");
-	}
-		
-				
-		if(cmd.getName().equalsIgnoreCase("start")) {
-			player.sendMessage("Well done!");
-		}
-		
-		return false;
 	}
 }
