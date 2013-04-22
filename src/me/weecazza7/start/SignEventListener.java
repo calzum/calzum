ed304624eeba041673ed283501fbd48e3a0cfef4
imjackjack <jack@Jacks_PC>
2013-04-22 19:01:04 +0100

    Xp

--- src/me/weecazza7/start/SignEventListener.java
+++ src/me/weecazza7/start/SignEventListener.java
@@ -26,18 +26,21 @@ public class SignEventListener implements Listener {
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e) {
 		Player p = e.getPlayer();
+		int getxp = p.getLevel();
 		if(!(e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
 		if (e.getClickedBlock().getState() instanceof Sign) {
 			Sign s = (Sign) e.getClickedBlock().getState();
 			if (s.getLine(0).equalsIgnoreCase("§4[Buy]")) {
-				if(p.getLevel()>=50)
-				e.getPlayer().sendMessage(ChatColor.GREEN + "You Got exp");
+				if(getxp>=50){
+				p.sendMessage(ChatColor.GREEN + "[Shop]You Bought a upgrade!");
+				p.setLevel(getxp-50);
 				return;
 			}{
-				e.getPlayer().sendMessage(ChatColor.GREEN + "You Got no exp");
+				e.getPlayer().sendMessage(ChatColor.RED + "[Shop]You do not have enough xp!");
 			}
 			}
 		}
 	}
+}
 
 
